31a8303ccd90408f67f11e3229cfcd91f62c9694
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-03-05 22:00:58 +0200

    add home page components

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./styles.scss";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+export const App = () => {
+  return <Home />;
+};
